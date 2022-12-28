4b1376493b3cea9f9739bae7dead8f724218f6eb
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2022-12-29 00:21:35 +0100

    active works

--- miflora_mqtt_daemon/__main__.py
+++ miflora_mqtt_daemon/__main__.py
@@ -1,21 +1,38 @@
 """ Description: Main entry point for the miflora-mqtt-daemon
 """
-
+from typing import Any
 import asyncio
-from datetime import date
+import json
+import platform
+from datetime import date, datetime
 from collections import OrderedDict
 from bleak import BleakScanner, BLEDevice, AdvertisementData
 from . import print_line, reporting_mode, sd_notifier, flores
 from .mqtt import start_mqtt, found_mqtt, send_mqtt
-from xiaomi_ble import XiaomiBluetoothDeviceData
+from .parser import XiaomiBluetoothDeviceData
+from home_assistant_bluetooth.models import BluetoothServiceInfoBleak as bti
 
 devices = {}
+
+if platform.system() == "Linux":
+    from bleak.assigned_numbers import AdvertisementDataType
+    from bleak.backends.bluezdbus.scanner import BlueZScannerArgs
+    from bleak.backends.bluezdbus.advertisement_monitor import OrPattern
+    # or_patterns is a workaround for the fact that passive scanning
+    # needs at least one matcher to be set. The below matcher
+    # will match all devices.
+    PASSIVE_SCANNER_ARGS = BlueZScannerArgs(
+        or_patterns=[
+            OrPattern(0, AdvertisementDataType.FLAGS, b"\x06"),
+            OrPattern(0, AdvertisementDataType.FLAGS, b"\x1a"),
+        ]
+    )
 
 
 async def print_device(device: BLEDevice, data: AdvertisementData):
     """ Print device information """
 
-    if device.name is not None and len(device.name)>0: #== "Flower care":
+    if device.name is not None and len(device.name) > 0:  # == "Flower care":
         flora = devices.get(device.address)
         if flora is None:
             devices[device.address] = device
@@ -24,15 +41,28 @@ async def print_device(device: BLEDevice, data: AdvertisementData):
                 flora = flores[device.address]
                 found_mqtt(f'{flora["name_pretty"]}@{flora["location_pretty"]}')
                 print_line(f"*** Adding device: {device}", warning=True)
-                flores[device.address].poller = 'bleak'
-                flores["detected"] = date.today()
+                flora.poller = 'bleak'
+                flora.detected = date.today()
+                flora.last_seen = date.today()
+                flora.data = XiaomiBluetoothDeviceData()
                 print_line(f"*** Added device: {device}", warning=True)
+
         if not flores.get(device.address) is None:
-            flores["last_seen"] = date.today()
-            flores[device.address].data = data
-            btData=XiaomiBluetoothDeviceData(data)
-            print_line(f"Data retrieved for {device.address}: {btData}")
-            send_mqtt(flores[device.address]["name_pretty"])
+            flora = flores[device.address]
+            serviceInfo=bti.from_device_and_advertisement_data(device, data, 'bleak',
+                                                   datetime.now().timestamp(),
+                                                   True)
+            print_line(f"uuids: {data.service_uuids}")
+            flora.data.update(serviceInfo)
+            flora.last_seen = date.today()
+
+            print_line(f"Data retrieved for {device.address}: {json.dumps(flora)} ")
+            vals={}
+            for key, value in flora.data._sensor_values.items():
+                vals[key.key] = value.native_value
+            print_line(json.dumps(vals))
+
+            send_mqtt(json.dumps(flora))
 
 
 async def main():
@@ -44,7 +74,15 @@ async def main():
 
     sd_notifier.notify('READY=1')
 
-    scanner = BleakScanner(print_device)
+    scanner_kwargs: dict[str, Any] = {"detection_callback": print_device}
+    scanner_kwargs["scanning_mode"] = "passive"
+    if platform.system() == "Linux":
+        if scanner_kwargs["scanning_mode"] == "passive":
+            scanner_kwargs["bluez"] = PASSIVE_SCANNER_ARGS
+
+    scanner = BleakScanner(**scanner_kwargs)
+
+
     print_line("Starting BLE passive scanner...")
     await scanner.start()
     print_line("BLE passive scanner started.")
